4a999e7f0a2071208470fad248b563727b4b98fd
abdolrahman kiany <94900618+abdoki7788@users.noreply.github.com>
2022-03-03 16:40:43 +0330

    add bootstrap and create navbar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,29 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/profile">profile</router-link>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <router-link class="navbar-brand" to="/">MY BLOG</router-link>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <router-link to="/" class="navbar-link">home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/about" class="navbar-link">about</router-link>
+          </li>
+        </ul>
+        <form class="d-flex">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
   </nav>
-  <router-view/>
+  <b-container>
+    <router-view/>
+  </b-container>
 </template>
 
 <style>
@@ -15,17 +34,18 @@
   text-align: center;
   color: #2c3e50;
 }
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
+.navbar-link {
   color: #2c3e50;
+  text-decoration: none;
+  margin: 0 0.5rem;
 }
 
-nav a.router-link-exact-active {
+.navbar-link:hover {
+  color: inherit;
+  opacity: 0.7;
+}
+
+.router-link-exact-active {
   color: #42b983;
 }
 </style>
