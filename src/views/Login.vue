6080e8f0850020edf56b98b46e5a40cd6d60a5e1
abdolrahman kiany <94900618+abdoki7788@users.noreply.github.com>
2022-03-04 08:54:53 +0330

    create login form

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,18 @@
 <template>
   <div class="login">
-    <h1>Login</h1>
-	<button class="btn btn-success" @click="doLogin()">login</button>
+	<form class="col-lg-6 mx-auto" @submit.prevent="doLogin()">
+		<h1>Login</h1>
+		<div class="mb-3">
+			<label for="usernameField" class="form-label">Username</label>
+			<input type="text" class="form-control" id="usernameField" aria-describedby="emailHelp" v-model="username">
+			<!-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> -->
+		</div>
+		<div class="mb-3">
+			<label for="passwordFiels" class="form-label">Password</label>
+			<input type="password" class="form-control" id="passwordFiels" v-model="password">
+		</div>
+		<button class="btn btn-success" type="submit">login</button>
+	</form>
   </div>
 </template>
 
@@ -9,10 +20,18 @@
 <script>
 export default {
 	name: 'login',
+	data() {
+		return {
+			username: '',
+			password: ''
+		}
+	},
 	methods: {
 		doLogin() {
-			this.$store.commit('Login', 'abc123')
-			this.$router.push('/')
+			// this.$store.commit('Login', 'abc123')
+			// this.$router.push('/')
+			console.log(this.username)
+			console.log(this.password)
 		}
 	}
 }
