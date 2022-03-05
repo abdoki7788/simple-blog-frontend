9f6f0a614bbe8c1bb11970fb7eac17f8093da030
abdolrahman kiany <94900618+abdoki7788@users.noreply.github.com>
2022-03-05 13:39:58 +0330

    created user profile page with qpi

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,5 +1,34 @@
 <template>
   <div class="profile">
     <h1>This is a profile page</h1>
+    <ul>
+      <li v-for="(value, key) in userdata">
+        {{ key }} : {{ value }}
+      </li>
+    </ul>
   </div>
 </template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'profile',
+  data() {
+    return {
+      userdata: {}
+    }
+  },
+  mounted() {
+    axios
+      .get('/api/auth/users/me')
+      .then(
+        response => this.userdata = response.data
+      )
+      .catch(error => {
+          console.log(error)
+          this.$store.commit('Logout')
+          this.$router.push('/login')
+        })
+  }
+}
+</script>
